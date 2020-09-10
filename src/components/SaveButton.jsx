6f10421992ccb9a595fac984c870baaf77900a08
Ken Ingram <ken.ingram@gmail.com>
2020-09-09 20:58:11 -0700

    Completing Save functionality which completes 100% of the MVP.

--- src/components/SaveButton.jsx
+++ src/components/SaveButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+
 /**
  * Take character info from props.data and store in airtable 'characters' table
  * 
@@ -9,41 +10,62 @@ import axios from 'axios';
  */
 
 function SaveButton(props) {
-  const { status, data } = props;
-  
-  async function saveRecord(){
-    axios.defaults.headers.common['Authorization'] = process.env.REACT_APP_AIRTABLE_KEY;
-    const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_AIRTABLE_BASE + "/characters";
-    // console.log(url);
+  const { status, data, classData, raceData } = props;
 
-    await axios.post(url, 
-      {
-        "records": [
-          {
-            "fields": {
-                playerName: data.playerName,
-                characterName: data.characterName,
-                strength: data.strength,
-                dexterity: data.dexterity,
-                intelligence: data.intelligence,
-                constitution: data.constitution,
-                wisdom: data.wisdom,
-                charisma: data.charisma,
-                characterClass: data.characterClass,
-                characterRace: data.characterRace
-            }
-          }
+  function classID(id){
+    const info = classData.find(obj => {
+      return obj.roll === id
+    });
+    return info;
+  }
+  
+  function raceID(id){
+    const info = raceData.find(obj => {
+      return obj.roll === id
+    });
+    return info;
+  }
+
+
+  async function saveRecord(e){
+    // axios.defaults.headers.common['Authorization'] = process.env.REACT_APP_AIRTABLE_KEY;
+    const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_AIRTABLE_BASE + "/characters";
+    // console.log("[SaveButton]: ",url)
+    // console.log(classData.find((roll, index, arr) => {Object.keys(arr[index])  }));
+    // console.log(classData.find((item, index) =>  console.log(index, item.roll)  ));
+
+    console.log(classID(data.characterClass).id);
+    console.log(raceID(data.characterRace).id);
+
+    const fields = {
+      playerName: data.playerName,
+      characterName: data.characterName,
+      strength: data.strength,
+      dexterity: data.dexterity,
+      intelligence: data.intelligence,
+      constitution: data.constitution,
+      wisdom: data.wisdom,
+      charisma: data.charisma,
+      class: [
+        classID(data.characterClass).id
+        ],
+      race: [
+        raceID(data.characterRace).id
         ]
-      },
+    }
+    // characterClass: data.characterClass,
+    // characterRace: data.characterRace
+    // console.log(fields);
+    await axios.post(url, { fields },
       {
-       Headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+       headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        'Content-Type': 'application/json'
        } 
       }      
     );
 
   }
-
 
   return (
     <div>
@@ -52,7 +74,7 @@ function SaveButton(props) {
       ? <button disabled>Save Character</button>
       : <button 
           onClick={
-            () => saveRecord()
+            (e) => saveRecord(e)
           }
         >Save Character</button>
     }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Test (props){
+  const [classData, setClassData] = useState([]);
+  const [raceData, setRaceData] = useState([]);
   // console.log(props.data);
+
+  async function classesID(){
+    const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_AIRTABLE_BASE + "/classes";
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+      }
+    });
+
+    const finalData = response.data.records.map((item) => ({'id': item.id, "roll": item.fields.roll_value, "className": item.fields.class_result}))
+    // console.log(finalData);
+    setClassData(finalData);
+    console.log(finalData);
+  }
+
+  async function racesID(){
+    const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_AIRTABLE_BASE + "/races";
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+      }
+    });
+
+    const finalData = response.data.records.map((item) => ({'id': item.id, "roll": item.fields.roll_value, "raceName": item.fields.race_result}))
+    // console.log(finalData);
+    setRaceData(finalData);
+    console.log(finalData);
+  }
+
+
+  
+  useEffect(() => {
+    classesID();
+    racesID();
+  }, []);
 
 
 return (
   <div>
     {
-     props.data  &&  props.data.id
+    //  (classData) ? classData.records.id : null
     }
   </div>
   )
